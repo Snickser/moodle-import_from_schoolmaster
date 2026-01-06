bc840cd82a3885bae9b8b1a787348824413f11df
root <snickser@gmail.com>
2026-01-06 10:54:46 +0000

    add

--- 4-certs_generate_for_users.php
+++ 4-certs_generate_for_users.php
@@ -15,7 +15,7 @@ use core\output\template_renderer;
 \core\session\manager::set_user(get_admin());
 force_current_language('uk'); 
 
-$filename = 'data-1753437909638.csv'; // Укажи имя или путь к CSV-файлу
+$filename = 'data-1767678688339.csv'; // Укажи имя или путь к CSV-файлу
 
 $data = new stdClass();
 
@@ -35,7 +35,11 @@ if (($handle = fopen($filename, 'r')) !== false) {
         die("Не удалось прочитать заголовки.\n");
     }
 
+$count = 0;
+
     while (($row = fgetcsv($handle, 0, ',')) !== false) {
+    $count++;
+    
         foreach ($headers as $i => $header) {
             $data->{$header} = $row[$i] ?? null;
         }
@@ -45,34 +49,48 @@ $rowid = str_pad($data->id, 3, '0', STR_PAD_LEFT);
 
 if (!$instance = $DB->get_record('tool_certificate_templates', [
     'name' => $rowid.'-'.$data->title])) {
-    echo "Шаблон ID: {$data->id} {$data->title} не найден\n";
+    echo "❌ Шаблон ID: {$data->id} {$data->title} не найден\n";
     continue;
 }
 
+if (!$user = $DB->get_record('user', [
+    'idnumber' => $data->idnumber, 'email' => $data->email])) {
+    echo "❌ Пользователь: {$data->email} {$data->idnumber} не найден\n";
+    continue;
+}
 
 $date = strtotime($data->issued_date);
 $date = userdate($date, '%d %B %Y');
 
-$template = \tool_certificate\template::instance(154);
+echo "$count Шаблон: {$instance->name} - {$user->email} ($date)\n";
 
-$coursid = 20;
-$userid = 3;
+$templateid = $instance->id;
+
+$template = \tool_certificate\template::instance($templateid);
+
+//echo print_r($user,true);
+$coursid = 1;
+
+$userid = $user->id;
+//$userid = 3;
 
 $object = [
     'courseid' => $coursid,
     'coursefullname' => '_name_',
     'coursecompletiondate' => $date,
-    'coursegrade' => $data->final_score,
+//    'coursegrade' => $data->final_score,
 ];
 
 
 $template->issue_certificate($userid, false, $object, 'mod_coursecertificate', $coursid);
 
+//echo print_r($template,true);
 
-echo "✅ Сертификат создан\n";
+//echo "✅ Сертификат создан\n";
 
 
-die;
+//die;
+//sleep(2);
 
     }
 
